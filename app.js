7dfa21cf36f92e913b011dd3b096557a3edf9843
Gopal Sasmal <gopalsasmal2000@gmail.com>
2024-03-19 11:53:24 +0530

    set cookies

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+require("dotenv").config();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require('express-session'); 
 const app = express();
 
 const homeRouter = require("./routes/home.routing");
@@ -10,7 +13,18 @@ const userRouter = require("./routes/user.routing");
  * @dev middleware space
  */
 app.use(express.json());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }));
+app.use(session({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production', 
+      httpOnly: true, 
+      maxAge: 1000 * 60 * 60 * 1,
+    },
+  }));
 app.use(morgan("tiny")); // Log request path
 app.disable("x-powered-by");
 app.use(function (req, res, next) {
--- controllers/Login.js
+++ controllers/Login.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const Login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    
+
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(401).json({ message: "Invalid email or password" });
@@ -15,9 +15,12 @@ const Login = async (req, res) => {
     if (!isMatch) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
-    
+
     const payload = { userId: user._id };
-    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: "1h" });
+    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1h",
+    });
+    req.session.token = token;
 
     res.json({ message: "Login successful", token });
   } catch (error) {
